86eaf943c8b36587f2cac27d8ad07e8c03e20dcc
Akshat3884 <59174408+Akshat3884@users.noreply.github.com>
2022-08-28 22:38:18 +0530

    centered cards

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,27 +2,15 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            src={
-              !imageUrl
-                ? "https://bitsofco.de/content/images/2018/12/broken-1.png"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
+        <div className="card">
+          <img src={!imageUrl?"https://bitsofco.de/content/images/2018/12/broken-1.png":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              className="btn btn-sm btn-secondary"
-            >
+            <a rel="noreferrer" href={newsUrl} className="btn btn-sm btn-secondary">
               Read More
             </a>
           </div>
